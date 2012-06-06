df07b3f9a15f1bd0650d435a72b81c8bbe6862af
abhirama <abhyrama@gmail.com>
2012-06-06 18:14:10 +0530

    Formatting and removed some old code.

--- mybot.js
+++ mybot.js
@@ -107,9 +107,9 @@ function decideBestMove(sortedMyMoves, sortedOpponentMoves) {
 }
 
 function sortSorroundingMoveCouts(sorroundingMoveCounts) {
-        sorroundingMoveCounts.sort(function(s0, s1){
+    sorroundingMoveCounts.sort(function(s0, s1){
             return s1.count - s0.count;
-        });
+            });
 }
 
 function filterSorroundingMoveCounts(sorroundingMoveCounts) {
@@ -142,7 +142,7 @@ function getMoves(node) {
 }
 
 function getOpponentMoves() {
-     return getMoves(new Node(get_opponent_x(), get_opponent_y()));
+    return getMoves(new Node(get_opponent_x(), get_opponent_y()));
 }
 
 
@@ -317,33 +317,33 @@ function getSorroundingCount(node, onlyConnectingOnes) {
     }
 
     if (!onlyConnectingOnes) {
-	    //Move north east
-	    if (isValidMove(x + 1, y - 1)) {
-		if (board[x + 1][y - 1] > 0) {
-		    count = count + 1;
-		}
-	    }
+        //Move north east
+        if (isValidMove(x + 1, y - 1)) {
+            if (board[x + 1][y - 1] > 0) {
+                count = count + 1;
+            }
+        }
 
-	    //Move south east
-	    if (isValidMove(x + 1, y + 1)) {
-		if (board[x + 1][y + 1] > 0) {
-		    count = count + 1;
-		}
-	    }
+        //Move south east
+        if (isValidMove(x + 1, y + 1)) {
+            if (board[x + 1][y + 1] > 0) {
+                count = count + 1;
+            }
+        }
 
-	    //Move south west
-	    if (isValidMove(x - 1, y + 1)) {
-		if (board[x - 1][y + 1] > 0) {
-		    count = count + 1;
-		}
-	    }
+        //Move south west
+        if (isValidMove(x - 1, y + 1)) {
+            if (board[x - 1][y + 1] > 0) {
+                count = count + 1;
+            }
+        }
 
-	    //Move north west
-	    if (isValidMove(x - 1, y - 1)) {
-		if (board[x - 1][y - 1] > 0) {
-		    count = count + 1;
-		}
-	    }
+        //Move north west
+        if (isValidMove(x - 1, y - 1)) {
+            if (board[x - 1][y - 1] > 0) {
+                count = count + 1;
+            }
+        }
     }
 
     //console.log(count);
@@ -354,126 +354,3 @@ function getSorroundingCount(node, onlyConnectingOnes) {
 function isValidMove(x, y) {
     return (x >= 0) && (y >= 0) && (x < WIDTH) && (y < HEIGHT);
 }
-
-
-function determineBestMove(x, y) {
-    var move = PASS;
-
-    var leastMoves = Number.MAX_VALUE; 
-    
-    var westMoveCount = goWestTillFruit(x, y);
-    //console.log('West move count:' + westMoveCount);
-
-    if ((westMoveCount != -1)) {
-        leastMoves = westMoveCount;
-        move = WEST;
-    }
-
-    var eastMoveCount = goEastTillFruit(x, y);
-    //console.log('East move count:' + eastMoveCount);
-
-    if ((eastMoveCount != -1) && (eastMoveCount < leastMoves)) {
-        leastMoves = eastMoveCount;
-        move = EAST; 
-    }
-
-    var northMoveCount = goNorthTillFruit(x, y);
-    //console.log('North move count:' + northMoveCount);
-
-    if ((northMoveCount != -1) && (northMoveCount < leastMoves)) {
-        leastMoves = northMoveCount;
-        move = NORTH;
-    }
-
-    var southMoveCount = goSouthTillFruit(x, y);
-    console.log('South move count:' + southMoveCount);
-
-    if ((southMoveCount != -1) && (southMoveCount < leastMoves)) {
-        move = SOUTH;
-    }
-
-    return move;
-}
-
-function goWestTillFruit(x, y) {
-    var board = get_board();
-    var start = x;
-    while (isValidMove(x = x - 1, y)) {
-        if (board[x][y] > 0) {
-            return start - x;
-        }
-    }
-    
-    return -1;
-
-}
-
-function goEastTillFruit(x, y) {
-    var board = get_board();
-    var start = x;
-    while (isValidMove(x = x + 1, y)) {
-        if (board[x][y] > 0) {
-            return x - start;
-        }
-    }
-
-    return -1;
-}
-
-function goNorthTillFruit(x, y) {
-    var board = get_board();
-    var start = y;
-    while (isValidMove(x, y = y - 1)) {
-        if (board[x][y] > 0) {
-            return start - y;
-        }
-    }
-
-    return -1;
-}
-
-function goSouthTillFruit(x, y) {
-    var board = get_board();
-    var start = y;
-    while (isValidMove(x, y = y + 1)) {
-        if (board[x][y] > 0) {
-            return y - start;
-        }
-    }
-
-    return -1;
-}
-
-function northHasFruit(x, y) {
-    if (isValidMove(x, y -1)) {
-        return get_board()[x, y - 1];
-    }
-
-    return 0;
-}
-
-function southHasFruit(x, y) {
-    if (y + 1 < HEIGHT) {
-        return get_board()[x, y + 1];
-    }
-
-    return 0;
-}
-
-function eastHasFruit(x, y) {
-    if (x + 1 < WIDTH) {
-        return get_board()[x + 1, y];
-    }
-
-    return 0;
-}
-
-function westHasFruit(x, y) {
-    if (x - 1 >= 0 ) {
-        return get_board()[x - 1, y];
-    }
-
-    return 0;
-}
-
-
