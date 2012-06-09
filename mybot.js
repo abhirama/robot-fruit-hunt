3c3f93744678415b125267a2019b81339ecebc46
abhirama <abhyrama@gmail.com>
2012-06-09 12:44:58 +0530

    Fixed another bug in the opponent nearer strategy.

--- mybot.js
+++ mybot.js
@@ -76,7 +76,7 @@ var MyBot = {
 
         //If there is not even a single fruit to which we are closer than the opponent, then move to the fruit which is furthest from the opponent
         if (!MyBot.isAtLeastOneFruitCloser()) {
-            return MyBot.sortedOpponentMoves[MyBot.sortedOpponentMoves.length - 1].direction;
+            MyBot.getMove(MyBot.position, MyBot.sortedOpponentMoves[MyBot.sortedOpponentMoves.length - 1].destinationNode);
         }
 
         var leastDistance = sortedMoves[0].distance; 
